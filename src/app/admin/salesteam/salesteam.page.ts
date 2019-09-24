bdc72f835e15cbc5898c09f39f7b465f5826b042
shahrul arief <shahrul.arief.sa@gmail.com>
2019-09-24 18:03:21 +0800

    update
    swipeable cards!!

--- src/app/admin/salesteam/salesteam.page.ts
+++ src/app/admin/salesteam/salesteam.page.ts
@@ -25,10 +25,10 @@ export class SalesteamPage implements OnInit {
   slider1 = {
     initialSlide: 0,
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 0,
     centeredSlides: true,
-    scrollbar: true,
-    pager: true,
+
+   
   };
  
   constructor(
@@ -45,42 +45,6 @@ export class SalesteamPage implements OnInit {
   }
 
   ngOnInit() {
-  }
-
-  //Move to Next slide
-  slideNext(object, slideView) {
-    slideView.slideNext(500).then(() => {
-      this.checkIfNavDisabled(object, slideView);
-    });
-  }
-
-  //Move to previous slide
-  slidePrev(object, slideView) {
-    slideView.slidePrev(500).then(() => {
-      this.checkIfNavDisabled(object, slideView);
-    });;
-  }
-
-  //Method called when slide is changed by drag or navigation
-  SlideDidChange(object, slideView) {
-    this.checkIfNavDisabled(object, slideView);
-  }
-
-  //Call methods to check if slide is first or last to enable disbale navigation  
-  checkIfNavDisabled(object, slideView) {
-    this.checkisBeginning(object, slideView);
-    this.checkisEnd(object, slideView);
-  }
-
-  checkisBeginning(object, slideView) {
-    slideView.isBeginning().then((istrue) => {
-      object.isBeginningSlide = istrue;
-    });
-  }
-  checkisEnd(object, slideView) {
-    slideView.isEnd().then((istrue) => {
-      object.isEndSlide = istrue;
-    });
   }
   ionViewWillEnter() {
     this.salesranking = [];
